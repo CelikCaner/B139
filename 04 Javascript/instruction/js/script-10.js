4d1c3865814c799e50716690fbc2f81fca0b6d0a
CelikCaner <gurthang90@gmail.com>
2023-05-03 20:30:24 +0200

    Html

--- 04 Javascript/instruction/js/script-10.js
+++ 04 Javascript/instruction/js/script-10.js
@@ -13,3 +13,9 @@ const menuItem3El = document.querySelector('ul.menu li:nth-child(3)');
 console.log(menuItem3El);
 
 headEl.innerText = "hello Js, no longer 'dommy'";
+
+//querySelectorAll() selects all the elements it matches
+const spanEls = document.querySelectorAll('#aboutUs span');
+console.log(spanEls);
+
+spanEls[0].innerText = 'HELLOOOO';
