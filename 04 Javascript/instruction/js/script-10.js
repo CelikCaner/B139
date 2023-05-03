85c1c135f6a44f8917ef187d0cea751f4336b583
CelikCaner <gurthang90@gmail.com>
2023-05-03 20:14:25 +0200

    Html

---
+++ 04 Javascript/instruction/js/script-10.js
@@ -0,0 +1,15 @@
+//querySelector() selects the first element it matches
+const headEl = document.querySelector('h1');
+console.log(headEl);
+
+const aboutUsEl = document.querySelector('#aboutUs');
+console.log(aboutUsEl);
+
+const spanEl = document.querySelector('#aboutUs span');
+console.log(spanEl);
+
+//querySelector() works with CSS selectors
+const menuItem3El = document.querySelector('ul.menu li:nth-child(3)');
+console.log(menuItem3El);
+
+headEl.innerText = "hello Js, no longer 'dommy'";
