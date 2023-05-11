a952e9cbf387458bdf16b190892ad41f01686dfb
CelikCaner <gurthang90@gmail.com>
2023-05-11 18:54:57 +0200

    Html

---
+++ My Projects/Calculator/script.js
@@ -0,0 +1,20 @@
+const buttons = document.querySelectorAll('.buttons button');
+const displayArea = document.querySelector('#displayArea');
+const equals = document.querySelector('#equals');
+
+const display = (event) => {
+  const clickedButton = event.target;
+  const clickedValue = clickedButton.innerText;
+  console.log(clickedValue);
+  displayArea.value += clickedValue;
+};
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', display);
+}
+
+// equals.addEventListener('click', () => {
+//   const operation = displayArea.value;
+//   const result = eval(operation);
+//   displayArea.value = result;
+// });
