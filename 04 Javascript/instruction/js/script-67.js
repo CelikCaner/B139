afc473995e760ef93397a5ea5f891aca3d29a53e
CelikCaner <gurthang90@gmail.com>
2023-05-19 21:31:18 +0200

    JS

--- 04 Javascript/instruction/js/script-67.js
+++ 04 Javascript/instruction/js/script-67.js
@@ -1,4 +1,4 @@
-import { mySort } from './modules/sort(script-67).js';
+import { numberSortASC, numberSortDESC } from './modules/sort(script-67).js';
 
 const arr1 = ['Ali', 'Veli', 'Selin', 'Şener', 'Leyla', 'Zeki', 'Metin'];
 const arr2 = [56, 13, 6, 134, 44, 12];
