eca9171f681c6aec49f9296ae98adfce61c9db75
CelikCaner <gurthang90@gmail.com>
2023-05-23 22:10:49 +0200

    JS

--- 04 Javascript/instruction/js/modules/tv-maze-api.js
+++ 04 Javascript/instruction/js/modules/tv-maze-api.js
@@ -1,10 +1,10 @@
 const baseUrl = 'https://api.tvmaze.com';
 
-const searchShows = (query) => {
+const searchShows = (query, callback) => {
   const url = `${baseUrl}/search/shows?q=${query}`;
   fetch(url)
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((data) => callback(data));
 };
 
 const getShowDetails = () => {};
--- 04 Javascript/instruction/js/script-71.js
+++ 04 Javascript/instruction/js/script-71.js
@@ -1,3 +1,35 @@
 import { searchShows } from '../js/modules/tv-maze-api.js';
+let timeoutSearch = null;
 
-searchShows('bank');
+document.getElementById('txtSearch').addEventListener('input', (event) => {
+  const query = event.target.value;
+
+  if (timeoutSearch) clearTimeout(timeoutSearch);
+  timeoutSearch = setTimeout(() => {
+    searchShows(query, (shows) => {
+      createMovies(shows);
+    });
+  }, 500);
+});
+
+const createMovies = (shows) => {
+  const lstTvShows = document.getElementById('lstTvShows');
+
+  lstTvShows.innerHTML = '';
+  shows.forEach((item) => {
+    const movieCard = createMovieCard(item);
+    lstTvShows.insertAdjacentHTML('afterbegin', movieCard);
+  });
+};
+
+const createMovieCard = (item) => {
+  return `    <div class="col">
+	<div class="card h-100">
+	  <img src="" class="card-img-top" alt="..." />
+	  <div class="card-body">
+		<h5 class="card-title">Card title</h5>
+		<p class="card-text">Film türleri</p>
+	  </div>
+	</div>
+  </div>`;
+};
