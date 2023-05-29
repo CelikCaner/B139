9517186af2fe712b710d76d561f31e9f63fc23be
CelikCaner <gurthang90@gmail.com>
2023-05-29 23:19:06 +0200

    HTML

--- My Projects/Calculator/script.js
+++ My Projects/Calculator/script.js
@@ -5,15 +5,13 @@ const equals = document.querySelector('#equals');
 const display = (event) => {
   const clickedButton = event.target;
   const clickedValue = clickedButton.innerText;
-  displayArea.value += clickedValue;
+  if (clickedValue === '=') {
+    displayArea.value = eval(displayArea.value);
+  } else {
+    displayArea.value += clickedValue;
+  }
 };
 
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', display);
 }
-
-// equals.addEventListener('click', () => {
-//   const operation = displayArea.value;
-//   const result = eval(operation);
-//   displayArea.value = result;
-// });
