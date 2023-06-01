aa2972ecb1b2a61bbdd16339c43e14fd69196dc2
CelikCaner <gurthang90@gmail.com>
2023-06-01 21:54:16 +0200

    React

--- 07 React/react-js/basics/src/App.js
+++ 07 React/react-js/basics/src/App.js
@@ -5,18 +5,29 @@ import Jsx2 from './components/02-jsx/02-jsx';
 import Jsx3 from './components/02-jsx/03-jsx';
 import Jsx4 from './components/02-jsx/04-jsx';
 import InlineStyling from './components/03-styles/01-inline-styling';
+import InternalStyling from './components/03-styles/02-internal-styling';
 
 export default function App() {
   return (
     <>
       <Apples />
+      <br />
       <Bananas1 />
+      <br />
       <Orange />
+      <br />
       <Jsx1 />
+      <br />
       <Jsx2 />
+      <br />
       <Jsx3 />
+      <br />
       <Jsx4 />
+      <br />
       <InlineStyling />
+      <br />
+      <InternalStyling />
+      <br />
     </>
   );
 }
