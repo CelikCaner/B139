8180f9f800aeb3d71b5075208f6a8f73632e0f94
CelikCaner <gurthang90@gmail.com>
2023-06-13 17:04:05 +0200

    React

---
+++ 07 React/react-js/basics/src/components/20-component-turleri/02-fonksiyon-turunde-componentler.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FonksiyonTurundeComponentler = () => {
+    return (
+        <div>FonksiyonTurundeComponentler</div>
+    )
+}
+
+export default FonksiyonTurundeComponentler
